0380f5d58581dcfff650b0f1d66d42731d98c45f
Av3z <willyanf17@hotmail.com>
2025-01-12 13:35:18 -0300

    added test to mapper

---
+++ test/src/services/utm_file/mapper/utm_model_mapper_test.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shorten_app/src/services/utm_file/mapper/utm_model_mapper.dart';
+
+void main() {
+  String json = '''
+  {
+    "medium": [
+        "fmkt",
+        "fut1179",
+        "fut1178",
+        "fut1164",
+        "fut1003",
+        "fut1006"
+	
+    ],
+    "source": [
+        "default",
+        "wp_channels"
+    ],
+    "campaign": [
+        "ra",
+        "pr2",
+        "pr3",
+        "ch3",
+        "ch5"
+    ]
+}
+  ''';
+
+  test('should convert json to a model', () {
+    final utmModel = UtmModelMapper.fromJson(jsonDecode(json));
+
+    expect(utmModel.medium[0], 'fmkt');
+  });
+}
