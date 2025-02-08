09c0e4b3932e2fa02c1106aefdce554e0d764e11
Av3z <willyanf17@hotmail.com>
2025-02-08 17:32:59 -0300

    updated a home page for use the new managers

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:shorten_app/src/core/shorten_manager/shorten_manager.dart';
 import 'package:shorten_app/src/services/copy_clipboard.dart';
 import 'package:shorten_app/src/services/utm_file/service/utm_file_service.dart';
 
@@ -12,10 +15,12 @@ class HomeController extends ChangeNotifier {
   ValueNotifier<String> selectedUtmSource = ValueNotifier('default');
   ValueNotifier<String> selectedUtmCampaign = ValueNotifier('pr2');
   ValueNotifier<String> urlMaked = ValueNotifier("");
+  ValueNotifier<String> shortenedUrl = ValueNotifier("");
 
   final UTMFileService _utmFileService;
+  final ShortenManager _shortenManager;
 
-  HomeController(this._utmFileService);
+  HomeController(this._utmFileService, this._shortenManager);
 
   List<String> utmMediumOptions = [];
   List<String> utmSourceOptions = [];
@@ -32,8 +37,36 @@ class HomeController extends ChangeNotifier {
     }
   }
 
-  void copyUrlToClipboard() {
+  void copyUrlToClipboard({bool isShortened = false}) {
     var copyClipboard = CopyClipboard();
+
+    if (isShortened) {
+      copyClipboard.copy(shortenedUrl.value);
+      return;
+    }
+
     copyClipboard.copy(urlMaked.value);
+  }
+
+  FutureOr<bool> shortenUrl() async {
+    return _shortenManager
+        .shorten(
+      "https://api-ssl.bitly.com/v4/shorten",
+      headers: {
+        'Authorization': 'Bearer 4336303230008b327901e7a1ff9e2a90af388cd2',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'long_url': urlMaked.value.toString(),
+      }),
+    )
+        .then((url) {
+      shortenedUrl.value = url;
+      copyUrlToClipboard();
+      return true;
+    }).catchError((error) {
+      log("Erro ao encurtar URL: $error");
+      return false;
+    });
   }
 }
